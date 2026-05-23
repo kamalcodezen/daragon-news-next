c0f288ae66e94110e979bc8e48d84200be6ed284
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-24 02:00:21 +0530

    login form create and design complete

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,28 +1,70 @@
+"use client";
+
 import "@/app/(auth)/login/login.css";
+import { p } from "framer-motion/client";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = (data) => {
+    // console.log(data, "login");
+    // console.log(errors, "errors");
+  };
+
+  // console.log(errors, "errors");
+
   return (
     <div className="w-11/12 mx-auto shadow rounded">
       <div className="min-h-[80vh] flex flex-col items-center justify-center">
-        <form className="space-y-4 ">
+        <form onSubmit={handleSubmit(handleLogin)} className="space-y-4">
           <p className="font-semibold text-orange-600">Login to Dashboard</p>
 
           <div className="input-group">
-            <input type="email" name="email" required />
+            <input
+              type="email"
+              {...register("email", { required: "kamal@exmaple.com" })}
+              placeholder=" "
+            />
             <label>Email Address</label>
           </div>
 
+          {errors.email && (
+            <p className="text-[10px] text-red-700">{errors.email?.message}</p>
+          )}
+
           <div className="input-group">
-            <input type="password" name="password" required />
+            <input
+              type="password"
+              placeholder=" "
+              {...register("password", {
+                required:
+                  "Must be at least 8 characters with 1 uppercase and 1 number",
+              })}
+            />
             <label>Password</label>
           </div>
 
-          <p className="text-[14px]  text-gray-600 underline hover:text-orange-600">
+          {errors.password && (
+            <p className="text-[10px] text-red-700">
+              {errors.password.message}
+            </p>
+          )}
+
+          <p className="text-[12px]  text-gray-600 underline hover:text-orange-600">
             <Link href="">Forgot password</Link>
           </p>
 
-          <button className="btn w-full bg-linear-to-t bg-gradient-to-r from-red-600 via-red-500 to-orange-500 text-white">
+          <button
+            type="submit"
+            className="btn w-full bg-linear-to-t bg-gradient-to-r from-red-600 via-red-500 to-orange-500 text-white"
+          >
             Login
           </button>
 
