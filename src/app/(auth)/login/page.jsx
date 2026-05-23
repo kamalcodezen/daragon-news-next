1e42d156e8ff3dee2bbe42bfc9b37d11181da0df
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-24 02:56:29 +0530

    login page color and position change

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -21,54 +21,65 @@ const LoginPage = () => {
   // console.log(errors, "errors");
 
   return (
-    <div className="w-11/12 mx-auto shadow rounded">
+    <div className="w-11/12 mx-auto">
       <div className="min-h-[80vh] flex flex-col items-center justify-center">
-        <form onSubmit={handleSubmit(handleLogin)} className="space-y-4">
-          <p className="font-semibold text-orange-600">Login to Dashboard</p>
+        <form
+          onSubmit={handleSubmit(handleLogin)}
+          className="  px-8 p-18 border border-transparent bg-gradient-to-r from-gray-200 via-gray-300 to-gray-100 rounded-2xl"
+        >
+          <p className="font-semibold text-orange-600  mb-7">
+            Login to Dashboard
+          </p>
 
-          <div className="input-group">
-            <input
-              type="email"
-              {...register("email", { required: "kamal@exmaple.com" })}
-              placeholder=" "
-            />
-            <label>Email Address</label>
+          <div className="mb-6">
+            <div className="input-group ">
+              <input
+                type="email"
+                {...register("email", { required: "kamal@exmaple.com" })}
+                placeholder=" "
+              />
+              <label>Email Address</label>
+            </div>
+
+            {errors.email && (
+              <p className="text-[10px] text-red-700 mt-2">
+                {errors.email?.message}
+              </p>
+            )}
           </div>
 
-          {errors.email && (
-            <p className="text-[10px] text-red-700">{errors.email?.message}</p>
-          )}
+          <div className="mb-2">
+            <div className="input-group ">
+              <input
+                type="password"
+                placeholder=" "
+                {...register("password", {
+                  required:
+                    "Must be at least 8 characters with 1 uppercase and 1 number",
+                })}
+              />
+              <label>Password</label>
+            </div>
 
-          <div className="input-group">
-            <input
-              type="password"
-              placeholder=" "
-              {...register("password", {
-                required:
-                  "Must be at least 8 characters with 1 uppercase and 1 number",
-              })}
-            />
-            <label>Password</label>
+            {errors.password && (
+              <p className="text-[10px] text-red-700 mt-2">
+                {errors.password.message}
+              </p>
+            )}
           </div>
 
-          {errors.password && (
-            <p className="text-[10px] text-red-700">
-              {errors.password.message}
-            </p>
-          )}
-
-          <p className="text-[12px]  text-gray-600 underline hover:text-orange-600">
+          <p className="text-[12px]  text-gray-600 underline hover:text-orange-600 mb-3">
             <Link href="">Forgot password</Link>
           </p>
 
           <button
             type="submit"
-            className="btn w-full bg-linear-to-t bg-gradient-to-r from-red-600 via-red-500 to-orange-500 text-white"
+            className="btn w-full bg-linear-to-t bg-gradient-to-r from-red-600 via-red-500 to-orange-500 text-white mb-3"
           >
             Login
           </button>
 
-          <p className="text-sm text-center text-gray-700">
+          <p className="text-sm text-center text-gray-700 ">
             Don't have an account{" "}
             <span className="text-orange-600">
               <Link href={"/register"}>Sign up</Link>
